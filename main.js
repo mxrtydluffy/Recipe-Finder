311eb83bd21032250bb35f1beb5c73a7bd0bac29
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-02-07 12:18:30 -0800

    [update] cleaned up js file

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
-// will probably change this later to getElementByID
-// but works for now
-let searchBtn = document.querySelector('#search')
+let searchBtn = document.getElementById('search')
 
 searchBtn.addEventListener('click', ()=>{
     console.log('Button Clicked!')
-    getAPI()
+    getRecipeData()
 })
 
 // using this API as a placeholder for now until I figure out how to
 // use the Tasty One.
 // praticing ways to use fetch
 // asynchronous function that'll fetch data from the edamam API
-async function getAPI(){
+async function getRecipeData(){
     let APP_ID = '696f4060'
     let APP_KEY = 'c94edefdff3f880d27188d0861d75a02'
     let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${APP_KEY}&q=soba`);
     console.log(response)
 	let data = await response.json()
 	console.log(data)
-	useAPI(data)
+	setRecipeData(data)
 }
+
 
 // function that'll do something with the data recieved from the API.
 // temporary html content to show API
-function useAPI(data){
+function setRecipeData(data){
 	document.querySelector('#content').innerHTML = `
 	<div class="card mx-auto mt-3" style="width: 230px;">
   		<img src="${data.hits[1].recipe.image}" class="card-img-top" alt="">
@@ -35,32 +34,3 @@ function useAPI(data){
 	</div>
 `
 }
-
-// Practicing:
-// fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=taco`), {
-//     method: 'GET',
-//     headers: {
-//         'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({
-//     })
-//     .then(res => {
-//         return res.JSON()
-//     })
-//     .then(data => console.log(data))
-//     .catch(error => console.log('ERROR'))
-// }
-
-
-// fetch('https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=taco', {
-//     method: 'GET',
-//     headers: {
-//         'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({
-//     })
-// }).then(res => {
-//    return res.json()
-// })
-//   .then(data => console.log(data))
-//   .catch(error => console.log('ERROR'))
