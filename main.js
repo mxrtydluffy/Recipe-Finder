294dc49410ca3e3fb1ffa9cdacb0cde65ce07b0c
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-02-07 01:18:55 -0800

    Revert "[update] connected to Tasty API"
    This reverts commit 1b5a2e816da9fc0bebe910bcf09282d8249cf9b5.

--- main.js
+++ main.js
@@ -1,63 +1,40 @@
-let searchButton = document.getElementById('search');
-
-searchButton.addEventListener('click', ()=>{
-    console.log('Button Clicked!')
-    sendApiRequest()
-})
-
-function sendApiRequest() {
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'f1ba02088fmshe08157a125fb4f7p1168b8jsn3f70935fed46',
-		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-	}
-};
-
-fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=tacos', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-}
-
-
 // will probably change this later to getElementByID
 // but works for now
-// let searchBtn = document.querySelector('#search')
+let searchBtn = document.querySelector('#search')
 
-// searchBtn.addEventListener('click', ()=>{
-//     console.log('Button Clicked!')
-//     getAPI()
-// })
+searchBtn.addEventListener('click', ()=>{
+    console.log('Button Clicked!')
+    getAPI()
+})
 
 // using this API as a placeholder for now until I figure out how to
 // use the Tasty One.
-// practicing ways to use fetch
+// praticing ways to use fetch
 // asynchronous function that'll fetch data from the edamam API
-// async function getAPI(){
-//     let APP_ID = '696f4060'
-//     let APP_KEY = 'c94edefdff3f880d27188d0861d75a02'
-//     let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${APP_KEY}&q=soba`);
-//     console.log(response)
-// 	let data = await response.json()
-// 	console.log(data)
-// 	useAPI(data)
-// }
+async function getAPI(){
+    let APP_ID = '696f4060'
+    let APP_KEY = 'c94edefdff3f880d27188d0861d75a02'
+    let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${APP_KEY}&q=soba`);
+    console.log(response)
+	let data = await response.json()
+	console.log(data)
+	useAPI(data)
+}
 
 // function that'll do something with the data recieved from the API.
 // temporary html content to show API
-// function useAPI(data){
-// 	document.querySelector('#content').innerHTML = `
-// 	<div class="card mx-auto mt-3" style="width: 230px;">
-//   		<img src="${data.hits[1].recipe.image}" class="card-img-top" alt="">
-//   	<div class="card-body">
-//     	<h5 class="card-title">${data.hits[1].recipe.label}</h5>
-//     	<p class="card-text">Source: ${data.hits[1].recipe.source}</p>
-//     	<a href="${data.hits[1].recipe.url}" class="btn btn-primary">Go To Recipe</a>
-//   	</div>
-// 	</div>
-// `
-// }
+function useAPI(data){
+	document.querySelector('#content').innerHTML = `
+	<div class="card mx-auto mt-3" style="width: 230px;">
+  		<img src="${data.hits[1].recipe.image}" class="card-img-top" alt="">
+  	<div class="card-body">
+    	<h5 class="card-title">${data.hits[1].recipe.label}</h5>
+    	<p class="card-text">Source: ${data.hits[1].recipe.source}</p>
+    	<a href="${data.hits[1].recipe.url}" class="btn btn-primary">Go To Recipe</a>
+  	</div>
+	</div>
+`
+}
 
 // Practicing:
 // fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${API_KEY}&q=taco`), {
