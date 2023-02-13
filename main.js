4be16c0f16340d4ed09db01deea4dda915ac935c
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-02-12 16:48:10 -0800

    [update] worked on js file

--- main.js
+++ main.js
@@ -1,36 +1,40 @@
-let searchBtn = document.getElementById('search-btn')
+const userSearchForm = document.querySelector('form');
+const usersSearchResults = document.querySelector('.searchResults');
+const container = document.querySelector('.container');
+let searchInput = '';
+const APP_ID = "696f4060";
+const APP_KEY = "c94edefdff3f880d27188d0861d75a02";
 
-searchBtn.addEventListener('click', ()=>{
-    console.log('Button Clicked!')
-    getRecipeData()
+userSearchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  searchInput = e.target.querySelector('input').value;
+  getRecipes();
 })
 
-// using this API as a placeholder for now until I figure out how to
-// use the Tasty One.
-// praticing ways to use fetch
-// asynchronous function that'll fetch data from the edamam API
-async function getRecipeData(){
-    let APP_ID = '696f4060'
-    let APP_KEY = 'c94edefdff3f880d27188d0861d75a02'
-    let response = await fetch(`https://api.edamam.com/search?app_id=${APP_ID}&app_key=${APP_KEY}&q=soba`);
-    console.log(response)
-	let data = await response.json()
-	console.log(data)
-	setRecipeData(data)
+async function getRecipes(){
+  const URL = `https://api.edamam.com/search?q=${searchInput}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=15`;
+  const response = await fetch(URL); 
+  const data = await response.json();
+  displayResults(data.hits)
+  console.log(data);
 }
 
-
-// // function that'll do something with the data recieved from the API.
-// // temporary html content to show API
-function setRecipeData(data){
-	document.querySelector('#content').innerHTML = `
-	<div class="card mx-auto mt-3" style="width: 230px;">
-  		<img src="${data.hits[1].recipe.image}" class="card-img-top" alt="">
-  	<div class="card-body">
-    	<h5 class="card-title">${data.hits[1].recipe.label}</h5>
-    	<p class="card-text">Source: ${data.hits[1].recipe.source}</p>
-    	<a href="${data.hits[1].recipe.url}" class="btn btn-primary">Go To Recipe</a>
-  	</div>
-	</div>
-`
+function displayResults(results){
+  container.classList.remove('container');
+  let resultsHTML= '';
+  results.map(result => {
+    resultsHTML += `
+      <div class="item">
+        <img src="${result.recipe.image}" alt="">
+        <div class="flex-container">
+          <h1 class="title">${result.recipe.label}</h1>
+          <a class="view-btn" target="_blank" href="${result.recipe.url}">View Recipe</a>
+        </div>
+        <p class="item-data">Calories: ${result.recipe.calories.toFixed(0)}</p>
+        <p class="item-data">Diet Label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : 'Sorry, No Data Found.'}</p>
+        <p class="item-data">Health Labels: ${result.recipe.healthLabels}</p>
+      </div>
+    `
+  })
+  usersSearchResults.innerHTML = resultsHTML;
 }
